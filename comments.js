bb21b099db3efd3f02d750e8df618d79959992e4
Svaen <s.sven@gmx.de>
2024-06-18 13:52:36 +0000

    chore: Remove unused code in comments.js

--- comments.js
+++ comments.js
@@ -4,10 +4,3 @@ var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-// create a router
-var router = express.Router();
